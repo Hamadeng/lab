563539b154a48a3c7ec61be91e6c1a7e144303ed
2172131431 <2172131431@CPES-3B1032-P11.ku.edu.kw>
2024-11-17 14:56:45 +0300

    changed num in array for function from (- 1) to (+ 5)

--- SCMLAB/src/p/Array.java
+++ SCMLAB/src/p/Array.java
@@ -10,7 +10,7 @@ public class Array {
 		} 
 		public int OCCUR( int key){ 
 		int count=0; 
-		for (int i=0; i < A.length ; i++)
+		for (int i=0; i < A.length + 5 ; i++)
 			if (A[i]==key) count++; 
 	return count; 
 	} 
