7c00ec4321ea8332cb3d8fb6abe76a7cc1c5636a
2172131431 <2172131431@CPES-3B1032-P11.ku.edu.kw>
2024-11-17 14:47:35 +0300

    array.java

--- SCMLAB/src/p/Array.java
+++ SCMLAB/src/p/Array.java
@@ -1,5 +1,17 @@
 package p;
 
-public class Array {
-
+public class Array { 
+	private static int [] A; 
+	public Array() { 
+		A = new int [10] ; 
+	}
+	public void setArray(int [] v){ 
+		A = v; 
+		} 
+		public int OCCUR( int key){ 
+		int count=0; 
+		for (int i=0; i < A.length - 1 ; i++) 
+			if (A[i]==key) count++; 
+	return count; 
+	} 
 }
